00a2dd383eab1c7907650a60505486232fde4680
David Henry <dw_henry@yahoo.com.au>
2012-07-17 21:41:19 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingGuest::Application.config.secret_token = '170c7a0b7c4d2c510125d9c5e84cc023d02f8f3fef074a65f01e422eafb8d7863f789397f5a47dfd7635d89a11cc54b0b8b0c7428f2b04471edefcdc48a0d40b'
