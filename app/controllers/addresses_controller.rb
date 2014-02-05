235cfe1ca375a1b8e782c0fd55239a7c2f9e4c8a
David Henry <dw_henry@yahoo.com.au>
2014-02-05 23:08:29 +0000

    Fix rvsps

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,6 @@
 class AddressesController < ApplicationController
   def index
     @wedding = Wedding.find(params[:wedding_id])
-    @rsvps = Rsvp.wheer(wedding_id: params[:wedding_id])
+    @rsvps = Rsvp.where(wedding_id: params[:wedding_id])
   end
 end
