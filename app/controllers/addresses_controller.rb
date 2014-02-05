49b11c6f5c832624a17b132a531750cc7bfba596
David Henry <dw_henry@yahoo.com.au>
2014-02-05 22:58:35 +0000

    Hack display of rsvps

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,19 +1,6 @@
 class AddressesController < ApplicationController
   def index
     @wedding = Wedding.find(params[:wedding_id])
-    @addresses = @wedding.addresses
-    @address = Address.new(:wedding => @wedding)
-  end
-
-  def create
-    address = Address.new(params[:address])
-    address.save!
-    render :partials => 'show', :locals => {:address => address}
-  rescue ActiveRecord::RecordInvalid
-    render :text => address.errors.to_hash, :status => 409
-  end
-
-  def destroy
-    Address.find(params[:id]).delete!
+    @rsvps = Rsvp.wheer(wedding_id: params[:wedding_id])
   end
 end
